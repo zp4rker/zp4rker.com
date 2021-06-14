82c9efcba0a7fe43b789dc776e16938336f2e59b
zp4rker <iam@zp4rker.com>
2021-06-14 13:01:18 +1000

    cleared some more warnings

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,24 +18,24 @@ const Navbar = () => {
   
     return (
       <nav className="navbar navbar-expand-lg navbar-light p-0">
-        <span className="navbar-toggler p-0 border-0 c-navbar-toggler" onClick={handleToggle}  id="navbarSideCollapse" aria-label="Toggle navigation">
+        <span className="navbar-toggler p-0 border-0 c-navbar-toggler" onClick={handleToggle} onKeyDown={handleToggle} role="button" tabIndex="0" id="navbarSideCollapse" aria-label="Toggle navigation">
           {!state.menu && <span className="navbar-toggler-icon"></span>}
           {state.menu && <span className="btn-close text-reset p-0" style={{ display: "block", width: "1.5em", height: "1.5em"}}></span>}
         </span>
         <div className={"navbar-collapse offcanvas-collapse "+(state.menu?'open':'')} id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-0 mb-lg-0">
             <li className="nav-item">
-              <Link to="/blog" onClick={handleToggleFalse} className="nav-link" activeClassName="active">blog </Link>
+              <Link to="/blog" onClick={handleToggleFalse} onKeyDown={handleToggleFalse} className="nav-link" activeClassName="active">blog </Link>
             </li>
             <li className="nav-item">
-              <Link to="/projects" onClick={handleToggleFalse} className="nav-link" activeClassName="active" href="#">projects</Link>
+              <Link to="/projects" onClick={handleToggleFalse} onKeyDown={handleToggleFalse} className="nav-link" activeClassName="active" href="#">projects</Link>
             </li>
             <li className="nav-item">
-              <Link to="/portofolio" onClick={handleToggleFalse} className="nav-link" activeClassName="active" href="#">portofolio</Link>
+              <Link to="/portofolio" onClick={handleToggleFalse} onKeyDown={handleToggleFalse} className="nav-link" activeClassName="active" href="#">portofolio</Link>
             </li>
           </ul>
         </div>
-        <Link to="/" onClick={handleToggleFalse} className="navbar-brand">zp4rker</Link>
+        <Link to="/" onClick={handleToggleFalse} onKeyDown={handleToggleFalse} className="navbar-brand">zp4rker</Link>
       </nav>
     );
   }
