380e7a9e1e8d9f9a98eed007699d821a4dd2c633
zp4rker <iam@zp4rker.com>
2021-06-17 14:16:50 +1000

    moved main element to content

--- src/components/content.js
+++ src/components/content.js
@@ -6,31 +6,33 @@ import { GlobalStateContext } from "../context/GlobalContextProvider"
 const Content = ({ centerContent, showFooter, children }) => {
     const state = useContext(GlobalStateContext)
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-12 py-2 py-sm-5 d-flex d-lg-block align-items-center">
-                    <div className="card w-100">
-                        <div className={"sub-c-card "+(state.menu?'open':'')}>
-                            <div className="card-head px-lg-4">
-                                <Navbar/>
-                            </div>
-                            <div className={"card-body px-4 px-lg-4 pb-4 " + (centerContent ? "d-flex justify-content-center align-items-center" : "")}>
-                                {/* Content */}
-                                <div className="content">
-                                    {children}
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 py-2 py-sm-5 d-flex d-lg-block align-items-center">
+                        <div className="card w-100">
+                            <div className={"sub-c-card "+(state.menu?'open':'')}>
+                                <div className="card-head px-lg-4">
+                                    <Navbar/>
                                 </div>
-                                {/* End Content */}
-                            </div>
-                            {showFooter ? (
-                                <div className="card-foot pb-3">
-                                    <Footer/>
+                                <div className={"card-body px-4 px-lg-4 pb-4 " + (centerContent ? "d-flex justify-content-center align-items-center" : "")}>
+                                    {/* Content */}
+                                    <div className="content">
+                                        {children}
+                                    </div>
+                                    {/* End Content */}
                                 </div>
-                            ) : null}
+                                {showFooter ? (
+                                    <div className="card-foot pb-3">
+                                        <Footer/>
+                                    </div>
+                                ) : null}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
   
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@ import Meta from "./meta"
 
 const Layout = ({ centerContent, showFooter, title, description, meta, children }) => {
     return (
-        <main>
+        <>
             <Meta title={title} description={description} meta={meta} />
             <Content centerContent={centerContent} showFooter={showFooter}>
                 {children}
             </Content>
-        </main>
+        </>
     )
 }
 
