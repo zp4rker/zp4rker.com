78475649aed334114c8090e4caf95bc9a775045b
zp4rker <iam@zp4rker.com>
2021-06-21 11:07:10 +1000

    implemented 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,14 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  )
+const NotFoundPage = () => {
+    return (
+        <Layout title="404: Page not found!" centerContent={true}>
+            <h1>Unable to find that page!</h1>
+            <p>Looks like you're a bit lost...</p>
+        </Layout>
+    )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
