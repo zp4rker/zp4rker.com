7dae690e27ae02df3d9c2ac46efebf9814c55f36
zp4rker <iam@zp4rker.com>
2021-06-25 14:20:59 +1000

    bam! posts page = done

---
+++ content/blog/new-look/index.md
@@ -0,0 +1,11 @@
+---
+title: New Look
+date: "2021-06-25T04:12:13.208Z"
+description: "My plans, aspirations and goals of this website."
+---
+
+*Bismillāh al-raḥmān al-raḥīm*
+
+So, I haven't really been actively posting for quite a while despite having a functional site up and I *was* blaming that on things such as not yet implementing my own design as well as not yet sorting out the domain routing issue however now I have implemented the redesign (not done as I'm writing this but will be once you read this), so that's one obstacle out of the way.
+
+The next step is to sort out the routing which will almost certainly be done by the time you read this. Anyways, enough babbling on about logistic stuff. The next **real** step is to start becoming an active blogposter and at this stage, I'll probably still be following on with my plans mentioned in the previous post.
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -4,10 +4,28 @@ import { useStaticQuery, graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
 const PostsPage = () => {
+    const posts = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+                nodes {
+                    excerpt(pruneLength: 155)
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        date(formatString: "Do MMMM YYYY")
+                        title
+                        description
+                    }
+                }
+            }
+        }
+    `).allMarkdownRemark.nodes
+
     return (
         <Layout title="Blog">
             <div id="topics">
-                <label className="title">Topics</label>
+                <h1 className="title">Topics</h1>
                 <div className="category">
                     {["Islām", "History", "Programming"].map((item, index) => {
                     return <Link key={index} to={"/category/"+item}>{item}</Link>
@@ -15,42 +33,23 @@ const PostsPage = () => {
                 </div>
             </div>
             <div id="articles">
-                <label className="title mt-3">Recent Posts</label>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">First Post</h2>
-                        <span>25th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
-                    <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">Second Post</h2>
-                        <span>26th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
-                    <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">Third Post</h2>
-                        <span>27th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
-                    <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">Fourth Post</h2>
-                        <span>28th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
-                    <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">Fifth Post</h2>
-                        <span>29th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
-                    <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
-                    <Link to="/posts"><article className="mt-3 mb-3">
-                        <h2 className="pb-0 mb-0 h5">Sixth Post</h2>
-                        <span>30th March 2021</span>
-                        <p className="pt-0 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quidem. Animi nobis libero, corporis error eligendi sint repellat ex ratione aliquid, minus, consectetur voluptatibus ab doloribus unde saepe labore dolor!</p>
-                    </article></Link>
+                <h1 className="title mt-3">Recent Posts</h1>
+                    {posts.map((post, index) => {
+                        var divider = <></>
+                        if (index < (posts.length - 1)) {
+                            divider = <span className="d-block p-0 m-0" style={{ borderTop: "1px solid #CCCCCC" }}></span>
+                        }
+                        return (
+                            <>
+                                <Link to={`/blog${post.fields.slug}`}><article className="mt-3 mb-3">
+                                    <h2 className="pb-0 mb-0 h5">{post.frontmatter.title}</h2>
+                                    <span>{post.frontmatter.date}</span>
+                                    <p className="pt-0 mt-0">{post.excerpt}</p>
+                                </article></Link>
+                                {divider}
+                            </>
+                        )
+                    })}
                 <div id="blog-viewall" className="pt-2">
                     <Link to="/blog/all">View all →</Link>
                 </div>
